88b82c315ea262bc3c8056a0a04e632bac8db509
Abraham Megantoro Samudra <18221123@mahasiswa.itb.ac.id>
2023-12-02 13:33:49 +0700

    Setup gofiber

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	"go-smart-dormitory/route"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
-	})
+	route.SetupRoutes(app)
 
 	app.Listen(":8080")
 }
---
+++ route/index.route.go
@@ -0,0 +1,11 @@
+package route
+
+import (
+	"go-smart-dormitory/handler"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func SetupRoutes(app *fiber.App) {
+	app.Get("/", handler.UserHandlerRead)
+}
