6dcd8b346bdee2cd98a00180d75a64ded8c80207
Abraham Megantoro Samudra <18221123@mahasiswa.itb.ac.id>
2023-12-07 08:56:54 +0700

    Feat: Add cors policy for cross route

--- route/index.route.go
+++ route/index.route.go
@@ -5,9 +5,17 @@ import (
 	"go-smart-dormitory/handler/middleware"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func SetupRoutes(app *fiber.App) {
+	app.Use(cors.New(cors.Config{
+		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin,Authorization",
+		AllowOrigins:     "*",
+		AllowCredentials: true,
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+	}))
+
 	// admin
 	app.Post("/login", handler.AdminHandlerLogin)
 
